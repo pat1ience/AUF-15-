1dc3a98b1b551cb8d4f12d756c420ae9a0b344a2
Azim <amangulovazim12@gmail.com>
2025-06-30 07:33:01 +0600

    fix AUF

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const inputNumber = document.getElementById('inputNumber');
 const result = document.querySelector('.result');
 const button = document.querySelector('.btn');
+const how = document.querySelector('.how')
 
 function calculateDiscount() {
     const inputValue = inputNumber.value;
     const percent = (inputValue * 15 / 100).toFixed(0);
     const overall = inputValue - percent;
     result.innerHTML = `Со скидкой: <br>${overall} сом`;
+    how.innerHTML = `(${percent})`
 }
 
 button.addEventListener('click', calculateDiscount);
